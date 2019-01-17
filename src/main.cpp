512e3676f3614d38aa514bf64dedb944462859ec
Diogo Justen <diogo.justen@daimler.com>
2019-01-17 17:57:22 +0100

    draft

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <arv.h>
 #include <string.h>
 #include <png.h>
+#include <glib-unix.h>
 
 using namespace std;
 
 static void arv_save_pgm(ArvBuffer * buffer, const char * filename)
 {
 		size_t buffer_size;
-		const char *framebuffer;
+		const void *framebuffer;
 		gchar *head;
 		gchar *buff;
 
@@ -16,7 +17,7 @@ static void arv_save_pgm(ArvBuffer * buffer, const char * filename)
 
 		head = g_strdup_printf ("P5\n %d\n %d\n 255\n", arv_buffer_get_image_height(buffer), arv_buffer_get_image_width(buffer));
 
-		buff = malloc(strlen(head) + buffer_size);
+		buff = (gchar *)malloc(strlen(head) + buffer_size);
 		memcpy (buff, head, strlen(head));
 		memcpy (buff + strlen(head), framebuffer, buffer_size);
 
@@ -26,33 +27,31 @@ static void arv_save_pgm(ArvBuffer * buffer, const char * filename)
 		free(buff);
 }
 
-static void new_buffer_cb (ArvStream *stream, void *user_data)
+static void new_buffer_cb (ArvStream *stream, int *frame_trigger)
 {
   ArvBuffer *buffer;
 
   buffer = arv_stream_try_pop_buffer (stream);
   if (buffer != NULL) {
     if (arv_buffer_get_status (buffer) == ARV_BUFFER_STATUS_SUCCESS) {
+			if (*frame_trigger == 0) {
+				gchar *filename = g_strdup_printf("/tmp/output%d.pgm", arv_buffer_get_frame_id(buffer));
+      	arv_save_pgm(buffer, filename);
+			}
+			(*frame_trigger)--;
 			arv_stream_push_buffer (stream, buffer);
 		}
   }
-}
-
-static void stream_cb (void *user_data, ArvStreamCallbackType type, ArvBuffer *buffer)
-{
-	if (type == ARV_STREAM_CALLBACK_TYPE_BUFFER_DONE) {
-		printf("stram_CB frameid: %d\n", arv_buffer_get_frame_id (buffer));
-	}
 }
 
 int main(int argc, char** argv)
 {
   ArvCamera *camera;
   ArvStream *stream;
-  ArvBuffer *buffer;
   gint payload;
   gint counter = 0;
   int i;
+	int frame_trigger = 10;
 
   arv_enable_interface("Fake");
 
@@ -61,46 +60,30 @@ int main(int argc, char** argv)
   camera = arv_camera_new ("Fake_1");
   g_assert (ARV_IS_CAMERA (camera));
 
-  stream = arv_camera_create_stream (camera, stream_cb, NULL);
+  stream = arv_camera_create_stream (camera, NULL, NULL);
   g_assert (ARV_IS_STREAM (stream));
 
   payload = arv_camera_get_payload (camera);
 
-	//for (i = 0; i <2; i++) {
-	//	arv_stream_push_buffer (stream,  arv_buffer_new (payload, NULL));
-	//}
+	arv_stream_push_buffer (stream,  arv_buffer_new (payload, NULL));
 
   arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_CONTINUOUS);
   arv_camera_start_acquisition(camera);
 
-	g_signal_connect (stream, "new-buffer", G_CALLBACK (new_buffer_cb), NULL);
+	g_signal_connect (stream, "new-buffer", G_CALLBACK (new_buffer_cb), &frame_trigger);
 	arv_stream_set_emit_signals (stream, TRUE);
 
-  buffer = arv_stream_pop_buffer (stream);
-
-  printf("height: %d\n", arv_buffer_get_image_height(buffer));
-  printf("width: %d\n", arv_buffer_get_image_width(buffer));
-
-	arv_save_pgm(buffer, "/tmp/png.pgm");
-
-	GMainLoop *loop = g_main_loop_new (NULL, FALSE);
-	printf("Antes do loop\n");
-	g_main_loop_run (loop);
-	printf("Depois do loop\n");
-
-	for (i = 0; i < 1; i++) {
-		buffer = arv_stream_pop_buffer (stream);
-
-		gchar *filename = g_strdup_printf("/tmp/output%d.pgm", i);
-		arv_save_pgm(buffer, filename);
-		g_free(filename);
+	while (frame_trigger >= 0) {
+		usleep(1000);
 	}
 
   arv_camera_stop_acquisition (camera);
 
-  g_clear_object (&buffer);
-  g_clear_object (&stream);
-  g_clear_object (&camera);
+	arv_stream_set_emit_signals (stream, FALSE);
+
+	g_object_unref (stream);
+	g_object_unref (camera);
+
   arv_shutdown();
 
   cout << "Done." << endl;
