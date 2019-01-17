6af64b544db6ce1e6783f640bd7e63baafc3c129
Diogo Justen <diogo.justen@daimler.com>
2019-01-17 13:03:35 +0100

    draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <arv.h>
+#include <string.h>
 
 using namespace std;
 
-#define GENICAM_FILENAME "/home/diogo/work/check_defects/src/arv-fake-camera.xml"
+#define GENICAM_FILENAME "/home/diogo/work/personal/check_defects/src/arv-fake-camera.xml"
 
 static void
 fill_pattern_cb (ArvBuffer *buffer, void *fill_pattern_data, guint32 exposure_time_us, guint32 gain, ArvPixelFormat pixel_format)
@@ -22,12 +23,20 @@ int main(int argc, char** argv)
   ArvBuffer *buffer;
   gint payload;
   gint counter = 0;
+  int i;
 
   arv_enable_interface("Fake");
 
   arv_update_device_list();
 
-  arv_set_fake_camera_genicam_filename(GENICAM_FILENAME);
+	unsigned int n_devices = arv_get_n_devices ();
+  for (i = 0; i < (int)n_devices; i++) {
+    printf("protocol: %s\n", arv_get_device_protocol (i));
+	}
+
+
+
+  //arv_set_fake_camera_genicam_filename(GENICAM_FILENAME);
 
   camera = arv_camera_new ("Fake_1");
   g_assert (ARV_IS_CAMERA (camera));
@@ -41,14 +50,19 @@ int main(int argc, char** argv)
   stream = arv_camera_create_stream (camera, NULL, NULL);
   g_assert (ARV_IS_STREAM (stream));
 
-  arv_fake_camera_set_fill_pattern (fake_camera, fill_pattern_cb, &counter);
+  //arv_fake_camera_set_fill_pattern (fake_camera, fill_pattern_cb, &counter);
 
   payload = arv_camera_get_payload (camera);
   printf("payload: %d\n", payload);
   arv_stream_push_buffer (stream,  arv_buffer_new (payload, NULL));
-  arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_SINGLE_FRAME);
+  //arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_SINGLE_FRAME);
+  arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_CONTINUOUS);
   arv_camera_start_acquisition (camera);
+  payload = arv_camera_get_payload (camera);
+  printf("payload: %d\n", payload);
+	printf("debug 1\n");
   buffer = arv_stream_pop_buffer (stream);
+	printf("debug 2\n");
   arv_camera_stop_acquisition (camera);
 
   arv_fake_camera_set_fill_pattern (fake_camera, NULL, NULL);
@@ -63,43 +77,31 @@ int main(int argc, char** argv)
   printf("pixelforma: %x\n", arv_buffer_get_image_pixel_format(buffer)); //ARV_PIXEL_FORMAT_MONO_8
 
 
-  size_t buffer_size;
-  //const void *framebuffer = arv_buffer_get_data (buffer, &buffer_size);
-  const char *framebuffer = (const char *) arv_buffer_get_data (buffer, &buffer_size);
-  printf("buffer size: %d\n", (int)buffer_size);
+	for (i = 0; i < 1; i++) {
+		size_t buffer_size;
+		//const void *framebuffer = arv_buffer_get_data (buffer, &buffer_size);
+		const char *framebuffer = (const char *) arv_buffer_get_data (buffer, &buffer_size);
+		printf("buffer size: %d\n", (int)buffer_size);
 
-  if(framebuffer) {
-    printf("boa famebuffer\n");
-  }
+		if(framebuffer) {
+			printf("boa famebuffer\n");
+		}
 
-  int i;
-  //for (i = 0; i < (int)buffer_size; i++) {
-  //  printf("content: %x \n", framebuffer[i]);
-  //}
+		//for (i = 0; i < (int)buffer_size; i++) {
+		//for (i = 0; i < 10; i++) {
+			printf("content: %x \n", framebuffer[145]);
+		//}
 
-  g_file_set_contents ("/tmp/output.raw", (const gchar *)framebuffer, buffer_size, NULL);
+		gchar *head = g_strdup_printf ("P5\n 512\n 512\n 255\n");
 
+		char *x = (char *)malloc(strlen(head) + buffer_size);
+		memcpy (x, head, strlen(head));
+		memcpy (x + strlen(head), framebuffer, buffer_size);
 
-  //--
-  const int dimx = 512, dimy = 512;
-  //int i;
-  int j;
-  FILE *fp = fopen("/tmp/first.ppm", "wb"); /* b - binary mode */
-  (void) fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
+		g_file_set_contents ("/tmp/output.pgm", (const gchar *)x, buffer_size, NULL);
+		//g_file_set_contents ("/tmp/output.pgm", (const gchar *)buf, buffer_size + strlen(head), NULL);
+	}
 
-  printf("%p\n", framebuffer);
-  for (j = 0; j < dimy; ++j)
-  {
-    for (i = 0; i < dimx; ++i)
-    {
-      static unsigned char color[3];
-      color[0] = i % 256;  /* red */
-      color[1] = j % 256;  /* green */
-      color[2] = (i * j) % 256;  /* blue */
-      (void) fwrite(color, 1, 3, fp);
-    }
-  }
-  (void) fclose(fp);
 
   g_clear_object (&buffer);
   g_clear_object (&stream);
