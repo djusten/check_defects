669d38ed0967061d6531b4865200d126036ca188
Diogo Justen <diogojusten@gmail.com>
2019-01-18 02:10:25 +0100

    draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,212 @@
-#include <iostream>
 #include <arv.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <png.h>
-#include <glib-unix.h>
+#include <limits.h>
+#include <ctype.h>
 
-using namespace std;
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <fstream>
+#include <iostream>
 
-static void arv_save_pgm(ArvBuffer * buffer, const char * filename)
+class ICamera {
+  public:
+    virtual ~ICamera() = default;
+
+    virtual bool open(int) = 0;
+    virtual void close() = 0;
+    virtual std::vector<char> getFramebuffer() = 0;
+    virtual void save_to_file(std::string, std::vector<char>) = 0;
+};
+
+class Camera : public ICamera {
+  public:
+    Camera();
+    virtual ~Camera() { close(); }
+
+    bool open(int) override;
+    void close() override;
+    std::vector<char> getFramebuffer() override;
+    void save_to_file(std::string, std::vector<char>) override;
+
+  private:
+    bool create(int);
+    bool initBuffers();
+
+    void stopCapture();
+    bool startCapture();
+
+    std::string getDeviceNameById(int id);
+
+    std::unique_ptr<ArvCamera> _camera;
+    std::unique_ptr<ArvStream> _stream;
+
+    unsigned int _payload{0};
+
+    int _width{0};
+    int _height{0};
+};
+
+Camera::Camera()
 {
-		size_t buffer_size;
-		const void *framebuffer;
-		gchar *head;
-		gchar *buff;
-
-		framebuffer = arv_buffer_get_data (buffer, &buffer_size);
-
-		head = g_strdup_printf ("P5\n %d\n %d\n 255\n", arv_buffer_get_image_height(buffer), arv_buffer_get_image_width(buffer));
-
-		buff = (gchar *)malloc(strlen(head) + buffer_size);
-		memcpy (buff, head, strlen(head));
-		memcpy (buff + strlen(head), framebuffer, buffer_size);
-
-		g_file_set_contents (filename, (const gchar *)buff, buffer_size + strlen(head), NULL);
-
-		free(head);
-		free(buff);
+  _camera = nullptr;
+  _stream = nullptr;
 }
 
-static void new_buffer_cb (ArvStream *stream, int *frame_trigger)
+void Camera::close()
 {
-  ArvBuffer *buffer;
+  if(_camera) {
+    stopCapture();
 
-  buffer = arv_stream_try_pop_buffer (stream);
-  if (buffer != NULL) {
-    if (arv_buffer_get_status (buffer) == ARV_BUFFER_STATUS_SUCCESS) {
-			if (*frame_trigger == 0) {
-				gchar *filename = g_strdup_printf("/tmp/output%d.pgm", arv_buffer_get_frame_id(buffer));
-      	arv_save_pgm(buffer, filename);
-			}
-			(*frame_trigger)--;
-			arv_stream_push_buffer (stream, buffer);
-		}
+    //FIXME
+    //g_object_unref(_camera);
+    //_camera = nullptr;
   }
+}
+
+std::string Camera::getDeviceNameById(int id)
+{
+  arv_update_device_list();
+
+  if((id >= 0) && (id < (int)arv_get_n_devices())) {
+    return arv_get_device_id(id);
+  }
+
+  return {};
+}
+
+bool Camera::create(int index)
+{
+  std::string deviceName = Camera::getDeviceNameById(index);
+  if (deviceName.size() > 0) {
+    _camera.reset(arv_camera_new(deviceName.c_str()));
+    return true;
+  }
+
+  return false;
+}
+
+bool Camera::initBuffers()
+{
+  if(_stream) {
+    //FIXME
+    //g_object_unref(stream);
+    //_stream = nullptr;
+  }
+
+  _stream.reset(arv_camera_create_stream(_camera.get(), NULL, NULL));
+  if (_stream) {
+    _payload = arv_camera_get_payload (_camera.get());
+
+    arv_stream_push_buffer(_stream.get(), arv_buffer_new(_payload, NULL));
+
+    return true;
+  }
+
+  return false;
+}
+
+bool Camera::open(int index)
+{
+  if(create(index)) {
+    return startCapture();
+  }
+  return false;
+}
+
+std::vector<char> Camera::getFramebuffer()
+{
+  char *_framebuffer = NULL;
+
+  if(_stream) {
+    ArvBuffer *arv_buffer = NULL;
+    int max_tries = 10;
+    int tries = 0;
+    for(; tries < max_tries; tries ++) {
+      arv_buffer = arv_stream_timeout_pop_buffer(_stream.get(), 200000);
+      if (arv_buffer != NULL && arv_buffer_get_status(arv_buffer) != ARV_BUFFER_STATUS_SUCCESS) {
+        arv_stream_push_buffer(_stream.get(), arv_buffer);
+      } else break;
+    }
+    if(arv_buffer != NULL && tries < max_tries) {
+      size_t buffer_size;
+      _framebuffer = (char *)arv_buffer_get_data(arv_buffer, &buffer_size);
+
+      arv_buffer_get_image_region(arv_buffer, NULL, NULL, &_width, &_height);
+
+      std::vector<char> raw_data;
+
+      raw_data.assign(_framebuffer, _framebuffer + buffer_size);
+
+      arv_stream_push_buffer(_stream.get(), arv_buffer);
+      return raw_data;
+    }
+  }
+
+  return {};
+}
+
+void Camera::stopCapture()
+{
+  arv_camera_stop_acquisition(_camera.get());
+
+  if(_stream) {
+    //FIXME
+    //g_object_unref(_stream);
+    //_stream = NULL;
+  }
+}
+
+bool Camera::startCapture()
+{
+  if(initBuffers() ) {
+    arv_camera_set_acquisition_mode(_camera.get(), ARV_ACQUISITION_MODE_CONTINUOUS);
+    arv_camera_start_acquisition(_camera.get());
+
+    return true;
+  }
+  return false;
+}
+
+void Camera::save_to_file(std::string filename, std::vector<char> data)
+{
+  gchar *head;
+  std::ofstream output_file(filename);
+
+  head = g_strdup_printf ("P5\n %d\n %d\n 255\n", _height, _width);
+
+  output_file << head;
+
+  for (int i = 0; i < (int) data.size(); i++)
+    output_file << data[i];
+
+  output_file.close();
+  g_free(head);
 }
 
 int main(int argc, char** argv)
 {
-  ArvCamera *camera;
-  ArvStream *stream;
-  gint payload;
-  gint counter = 0;
-  int i;
-	int frame_trigger = 0;
-
-	if (argc > 1) {
-		frame_trigger = strtol (argv[1], NULL, 0);
-	}
+  int index = 0;
 
   arv_enable_interface("Fake");
 
-  arv_update_device_list();
+  ICamera *cam = new Camera();
 
-  camera = arv_camera_new ("Fake_1");
-  g_assert (ARV_IS_CAMERA (camera));
+  if (!cam->open(index)) {
+    printf("Error\n");
+  } else {
 
-  stream = arv_camera_create_stream (camera, NULL, NULL);
-  g_assert (ARV_IS_STREAM (stream));
+    std::vector<char> data = cam->getFramebuffer();
 
-  payload = arv_camera_get_payload (camera);
+    cam->save_to_file("/tmp/file.pgm", data);
 
-	arv_stream_push_buffer (stream,  arv_buffer_new (payload, NULL));
-
-  arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_CONTINUOUS);
-  arv_camera_start_acquisition(camera);
-
-	g_signal_connect (stream, "new-buffer", G_CALLBACK (new_buffer_cb), &frame_trigger);
-	arv_stream_set_emit_signals (stream, TRUE);
-
-	while (frame_trigger >= 0) {
-		usleep(1000);
-	}
-
-  arv_camera_stop_acquisition (camera);
-
-	arv_stream_set_emit_signals (stream, FALSE);
-
-	g_object_unref (stream);
-	g_object_unref (camera);
+    cam->close();
+  }
 
   arv_shutdown();
-
-  cout << "Done." << endl;
 
   return 0;
 }
--- src/main2.cpp
+++ src/main2.cpp
@@ -1,218 +1,96 @@
-#include <arv.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <ctype.h>
-
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <memory>
-#include <fstream>
 #include <iostream>
+#include <arv.h>
+#include <string.h>
+#include <png.h>
+#include <glib-unix.h>
 
-class ICamera {
-  public:
-    virtual ~ICamera() = default;
+using namespace std;
 
-    virtual bool open(int) = 0;
-    virtual void close() = 0;
-    virtual std::vector<char> getFramebuffer() = 0;
-    virtual void save_to_file(std::string, std::vector<char>) = 0;
-};
-
-class Camera : public ICamera {
-  public:
-    Camera();
-    virtual ~Camera() { close(); }
-
-    bool open(int) override;
-    void close() override;
-    std::vector<char> getFramebuffer() override;
-    void save_to_file(std::string, std::vector<char>) override;
-
-  private:
-    bool create(int);
-    bool initBuffers();
-
-    void stopCapture();
-    bool startCapture();
-
-    std::string getDeviceNameById(int id);
-
-    std::unique_ptr<ArvCamera> _camera;
-    std::unique_ptr<ArvStream> _stream;
-
-    unsigned int _payload{0};
-
-    int _width{0};
-    int _height{0};
-};
-
-Camera::Camera()
+static void arv_save_pgm(ArvBuffer * buffer, const char * filename)
 {
-  _camera = nullptr;
-  _stream = nullptr;
+		size_t buffer_size;
+		const void *framebuffer;
+		gchar *head;
+		gchar *buff;
+
+		framebuffer = arv_buffer_get_data (buffer, &buffer_size);
+
+		head = g_strdup_printf ("P5\n %d\n %d\n 255\n", arv_buffer_get_image_height(buffer), arv_buffer_get_image_width(buffer));
+
+		buff = (gchar *)malloc(strlen(head) + buffer_size);
+		memcpy (buff, head, strlen(head));
+		memcpy (buff + strlen(head), framebuffer, buffer_size);
+
+		g_file_set_contents (filename, (const gchar *)buff, buffer_size + strlen(head), NULL);
+
+		free(head);
+		free(buff);
 }
 
-void Camera::close()
+static void new_buffer_cb (ArvStream *stream, int *frame_trigger)
 {
-  if(_camera) {
-    stopCapture();
+  ArvBuffer *buffer;
 
-    //FIXME
-    //g_object_unref(_camera);
-    //_camera = nullptr;
+  buffer = arv_stream_try_pop_buffer (stream);
+  if (buffer != NULL) {
+    if (arv_buffer_get_status (buffer) == ARV_BUFFER_STATUS_SUCCESS) {
+			if (*frame_trigger == 0) {
+				gchar *filename = g_strdup_printf("/tmp/output%d.pgm", arv_buffer_get_frame_id(buffer));
+      	arv_save_pgm(buffer, filename);
+			}
+			(*frame_trigger)--;
+			arv_stream_push_buffer (stream, buffer);
+		}
   }
-}
-
-std::string Camera::getDeviceNameById(int id)
-{
-  arv_update_device_list();
-
-  if((id >= 0) && (id < (int)arv_get_n_devices())) {
-    return arv_get_device_id(id);
-  }
-
-  return {};
-}
-
-bool Camera::create(int index)
-{
-  std::string deviceName = Camera::getDeviceNameById(index);
-  if (deviceName.size() > 0) {
-    _camera.reset(arv_camera_new(deviceName.c_str()));
-    return true;
-  }
-
-  return false;
-}
-
-bool Camera::initBuffers()
-{
-  if(_stream) {
-    //FIXME
-    //g_object_unref(stream);
-    //_stream = nullptr;
-  }
-
-  _stream.reset(arv_camera_create_stream(_camera.get(), NULL, NULL));
-  if (_stream) {
-    _payload = arv_camera_get_payload (_camera.get());
-
-    arv_stream_push_buffer(_stream.get(), arv_buffer_new(_payload, NULL));
-
-    return true;
-  }
-
-  return false;
-}
-
-bool Camera::open(int index)
-{
-  if(create(index)) {
-    return startCapture();
-  }
-  return false;
-}
-
-std::vector<char> Camera::getFramebuffer()
-{
-  char *_framebuffer = NULL;
-
-  if(_stream) {
-    ArvBuffer *arv_buffer = NULL;
-    int max_tries = 10;
-    int tries = 0;
-    for(; tries < max_tries; tries ++) {
-      arv_buffer = arv_stream_timeout_pop_buffer(_stream.get(), 200000);
-      if (arv_buffer != NULL && arv_buffer_get_status(arv_buffer) != ARV_BUFFER_STATUS_SUCCESS) {
-        arv_stream_push_buffer(_stream.get(), arv_buffer);
-      } else break;
-    }
-    if(arv_buffer != NULL && tries < max_tries) {
-      size_t buffer_size;
-      _framebuffer = (char *)arv_buffer_get_data(arv_buffer, &buffer_size);
-
-      arv_buffer_get_image_region(arv_buffer, NULL, NULL, &_width, &_height);
-
-      std::vector<char> raw_data;
-
-      raw_data.assign(_framebuffer, _framebuffer + buffer_size);
-
-      arv_stream_push_buffer(_stream.get(), arv_buffer);
-      return raw_data;
-    }
-  }
-
-  return {};
-}
-
-void Camera::stopCapture()
-{
-  arv_camera_stop_acquisition(_camera.get());
-
-  if(_stream) {
-    //FIXME
-    //g_object_unref(_stream);
-    //_stream = NULL;
-  }
-}
-
-bool Camera::startCapture()
-{
-  if(initBuffers() ) {
-    arv_camera_set_acquisition_mode(_camera.get(), ARV_ACQUISITION_MODE_CONTINUOUS);
-    arv_camera_start_acquisition(_camera.get());
-
-    return true;
-  }
-  return false;
-}
-
-void Camera::save_to_file(std::string filename, std::vector<char> data)
-{
-  gchar *head;
-  std::ofstream output_file(filename);
-
-  head = g_strdup_printf ("P5\n %d\n %d\n 255\n", _height, _width);
-
-  output_file << head;
-
-  for (int i=0; i<data.size(); i++)
-    output_file << data[i];
-
-  output_file.close();
-  g_free(head);
 }
 
 int main(int argc, char** argv)
 {
-  int index = 0;
+  ArvCamera *camera;
+  ArvStream *stream;
+  gint payload;
+  gint counter = 0;
+  int i;
+	int frame_trigger = 0;
 
-  int frame_trigger = 0;
-
-  if (argc > 1) {
-    frame_trigger = strtol (argv[1], NULL, 0);
-  }
+	if (argc > 1) {
+		frame_trigger = strtol (argv[1], NULL, 0);
+	}
 
   arv_enable_interface("Fake");
 
-  ICamera *cam = new Camera();
+  arv_update_device_list();
 
-  if (!cam->open(index)) {
-    printf("Error\n");
-  } else {
+  camera = arv_camera_new ("Fake_1");
+  g_assert (ARV_IS_CAMERA (camera));
 
-    std::vector<char> data = cam->getFramebuffer();
+  stream = arv_camera_create_stream (camera, NULL, NULL);
+  g_assert (ARV_IS_STREAM (stream));
 
-    cam->save_to_file("/tmp/file.pgm", data);
+  payload = arv_camera_get_payload (camera);
 
-    cam->close();
-  }
+	arv_stream_push_buffer (stream,  arv_buffer_new (payload, NULL));
+
+  arv_camera_set_acquisition_mode (camera, ARV_ACQUISITION_MODE_CONTINUOUS);
+  arv_camera_start_acquisition(camera);
+
+	g_signal_connect (stream, "new-buffer", G_CALLBACK (new_buffer_cb), &frame_trigger);
+	arv_stream_set_emit_signals (stream, TRUE);
+
+	while (frame_trigger >= 0) {
+		usleep(1000);
+	}
+
+  arv_camera_stop_acquisition (camera);
+
+	arv_stream_set_emit_signals (stream, FALSE);
+
+	g_object_unref (stream);
+	g_object_unref (camera);
 
   arv_shutdown();
+
+  cout << "Done." << endl;
 
   return 0;
 }
