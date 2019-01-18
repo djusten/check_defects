6a4b20992493c21bed3c48197154a51cb1109121
Diogo Justen <diogo.justen@daimler.com>
2019-01-18 12:11:25 +0100

    Removed unnecessary stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 #include <arv.h>
 
-using namespace std;
-
-#define GENICAM_FILENAME "genicam.xml"
-
 int main(int argc, char** argv)
 {
   arv_enable_interface("Fake");
 
   arv_update_device_list();
 
-  arv_set_fake_camera_genicam_filename(GENICAM_FILENAME);
-
   arv_shutdown();
 
-  cout << "Done." << endl;
+  std::cout << "Done." << std::endl;
 
   return 0;
 }
